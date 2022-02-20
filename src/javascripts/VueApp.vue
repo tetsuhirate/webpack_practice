96151431511f79bade4dbc8a7b94bf49e486048b
tetsu <hirate1969@gmail.com>
2022-02-20 22:41:04 +0900

    ES6, React Vue, etc....

---
+++ src/javascripts/VueApp.vue
@@ -0,0 +1,20 @@
+<template>
+<p>{{message}}</p>
+</template>
+
+<script>
+export default ({
+    data: () => {
+        console.log('Vuejs is instaled!!');
+        return {
+            message :'Hey, thisi Vue!',
+        };
+    },
+});
+</script>
+
+<style scoped>
+p{
+    color:green;
+}
+</style>
