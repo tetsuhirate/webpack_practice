0b5eb9c1dd039cf47ec96dd7798d8a7fd6e37e9f
tetsu <hirate1969@gmail.com>
2022-02-20 23:07:22 +0900

    Install TypeScript

--- src/javascripts/reactApp.jsx
+++ src/javascripts/reactApp.jsx
@@ -1,10 +1,12 @@
 import ReactDom from 'react-dom';
 import * as React from 'react';
+import Alert from './Alert.tsx';
 
 const App = (props) => {
     return (
         <div>
             Hello, React App!
+            <Alert message="Success!" />
         </div>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,15 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.(ts|tsx)/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    }
+                ]
+            },
+            {
                 test: /\.vue/,
                 exclude: /node_modules/,
                 use:[
